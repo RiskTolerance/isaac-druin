5c8edce05a9a716e92a0844dac098e7d62671bbb
Isaac Druin <isaac.druin@gmail.com>
2025-04-30 21:18:42 -0500

    start drafting content

---
+++ src/lib/blocks/home/Projects.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Project from '../projects/Project.svelte';
+	const projects = [
+		{
+			title: 'Project 1',
+			description: 'Description of project 1',
+			image: 'https://picsum.photos/200/300',
+			link: '/'
+		},
+		{
+			title: 'Project 2',
+			description: 'Description of project 2',
+			image: 'https://picsum.photos/200/300',
+			link: '/'
+		},
+		{
+			title: 'Project 3',
+			description: 'Description of project 3',
+			image: 'https://picsum.photos/200/300',
+			link: '/'
+		}
+	];
+</script>
+
+<div class="container mx-auto">
+	<p>Projects</p>
+	{#each projects as project}
+		<Project {...project}></Project>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Blog from '$lib/blocks/home/Blog.svelte';
+	import Felix from '$lib/blocks/home/Felix.svelte';
+	import Hero from '$lib/blocks/home/Hero.svelte';
+	import Projects from '$lib/blocks/home/Projects.svelte';
+</script>
+
+<Hero></Hero>
+<Projects></Projects>
+<Felix></Felix>
+<Blog></Blog>
