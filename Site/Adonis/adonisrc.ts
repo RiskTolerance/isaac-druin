8068ae6ec00969b8f705bb54c0c4c86a0eb98889
Isaac Druin <isaac.druin@gmail.com>
2025-05-27 07:43:53 -0500

    idk what im doing

--- Site/Adonis/adonisrc.ts
+++ Site/Adonis/adonisrc.ts
@@ -46,7 +46,8 @@ export default defineConfig({
     () => import('@adonisjs/core/providers/vinejs_provider'),
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
-    () => import('@adonisjs/auth/auth_provider')
+    () => import('@adonisjs/auth/auth_provider'),
+    () => import('@adonisjs/vite/vite_provider')
   ],
 
   /*
@@ -83,4 +84,12 @@ export default defineConfig({
     ],
     forceExit: false,
   },
+  metaFiles: [{
+    pattern: 'public/**',
+    reloadServer: false,
+  }],
+  hooks: {
+    onBuildStarting: [() => import('@adonisjs/vite/build_hook')]
+  },
+    assetsBundler: false
 })
--- src/routes/blog/[slug]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-	<p>Post</p>
-</div>
