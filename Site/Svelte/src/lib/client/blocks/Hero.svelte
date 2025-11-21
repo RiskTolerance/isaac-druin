f468deb521fe5744eefdd3f1d84e3e26b14a3d53
Isaac Druin <isaac.druin@gmail.com>
2025-11-21 06:21:47 -0600

    basic implementation for Hero text scroll

--- Site/Svelte/src/lib/client/blocks/Hero.svelte
+++ Site/Svelte/src/lib/client/blocks/Hero.svelte
@@ -1,26 +1,68 @@
 <script lang="ts">
+	import gsap from 'gsap';
+
+	import { horizontalLoop } from '$client/utils/seamlessLoop';
+	import { onMount } from 'svelte';
+
+	const titles = [
+		'Full Stack Web Developer',
+		'Graphic Designer',
+		'UX|UI Nerd',
+		'3D Artist',
+		'Photographer'
+	];
+
+	const tools = [
+		'SvelteKit',
+		'Illustrator',
+		'Photoshop',
+		'Blender',
+		'Tailwind CSS',
+		'Docker',
+		'Cloudflare'
+	];
+
+	let titlesContainer: HTMLElement;
+	let toolsContainer: HTMLElement;
+
+	onMount(() => {
+		const titlesTimeline = horizontalLoop(titlesContainer.children);
+		const toolsTimeline = horizontalLoop(toolsContainer.children);
+
+		titlesTimeline.play().repeat(-1);
+		toolsTimeline.play().repeat(-1);
+	});
 </script>
 
 <div class="font-display container mx-auto pl-[calc(20%)]">
 	<p>Hi! My name is</p>
-	<p style="font-size: 4rem; padding-left: 2rem;">Isaac Druin</p>
+	<p class="text-5xl">Isaac Druin</p>
 	<div class="h-12"></div>
 	<p>I am a</p>
-	<p>
-		Full Stack Web Developer <span>&&</span> Graphic Designer <span>&&</span> UX|UI Nerd
-		<span>&&</span> 3D Artist <span>&&</span> Photographer
-	</p>
+	<div bind:this={titlesContainer} class="flex items-center space-x-4">
+		{#each titles as title, idx}
+			<p class="text-5xl text-nowrap">{title}</p>
+			{#if idx < titles.length - 1}
+				<p><span class="text-xl">&&</span></p>
+			{/if}
+		{/each}
+	</div>
+
 	<p>using</p>
-	<p>
-		SvelteKit <span>&&</span> Illustrator <span>&&</span> Photoshop <span>&&</span> Blender
-		<span>&&</span>
-		Sanity CMS<span>&&</span> Docker <span>&&</span> Cloudflare
-	</p>
+	<div bind:this={toolsContainer} class="flex items-center gap-4">
+		{#each tools as tool, idx}
+			<p class="text-5xl text-nowrap">{tool}</p>
+			{#if idx < tools.length - 1}
+				<p><span class="text-xl">&&</span></p>
+			{/if}
+		{/each}
+	</div>
+
 	<p>to craft</p>
-	<p style="font-size: 2.5rem; padding-left: 2rem;">Slightly above average web stuff</p>
+	<p class="text-5xl">Slightly above average web stuff</p>
 </div>
 
-<style>
+<!-- <style>
 	p {
 		font-size: 3rem;
 		text-wrap: nowrap;
@@ -38,4 +80,4 @@
 		top: -0.2em;
 		color: #ff3e00;
 	}
-</style>
+</style> -->
