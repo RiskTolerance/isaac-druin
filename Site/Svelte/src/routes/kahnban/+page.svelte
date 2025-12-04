c05ae62ef4fc2e966d5ed22fde867f51ea34ca16
Isaac Druin <isaac.druin@gmail.com>
2025-12-04 07:29:26 -0600

    kahnban route

---
+++ Site/Svelte/src/routes/kahnban/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+</script>
+
+<div>
+	<h1>Live Project Tracking</h1>
+	<p>I recently saw a meme that goes something like this:</p>
+	<ul>
+		<li>First Frame: "How are you so knowledgable about so many things?""</li>
+		<li>Second Frame: "I've quit more things than you've started.""</li>
+	</ul>
+	<p>
+		And that meme really resonated with me. I'm an ideas guy. Having the idea, conceptualizing,
+		planning, and starting are the easiest parts for me. Where it gets difficult is seeing the
+		project through. Not because the project is hard or I run into a wall, or I'm not motivated, but
+		because I usually have another idea that captivates me before I finish. That last 10% being 90%
+		of the work is real.
+	</p>
+	<p>
+		And so, I need to keep track of my projects and ideas. Since I'm already writing about what I'm
+		up to here, it makes sense to park and track my ideas here as well. So that's the plan - this
+		Khanban board is a high-level overview of my planned and in-progress projects. I might even keep
+		ideas for blog posts here.
+	</p>
+</div>
+
+<div>
+	<h2>Projects (in order of "hell yeah")</h2>
+	<ul>
+		<li>Personal site for blogging and project tracking.</li>
+		<li>AI Dog Collar</li>
+		<li>AI Stock Trader</li>
+		<li>
+			<ol>
+				<li>CMS Deep Dive (Directus, first!)</li>
+				<li>
+					Building a company full-stack site w/ CMS and database on a cluster of three Rasberry Pis
+				</li>
+			</ol>
+		</li>
+		<li>
+			Blogging/Vlogging project: build a web developement company starting with <strong
+				>NOTHING</strong
+			> but $100 and an internet connection ($100 initially goes to a computer setup). Only using free
+			tools - Linux, self-hosting, etc.
+		</li>
+	</ul>
+</div>
